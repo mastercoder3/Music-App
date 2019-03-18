f1e16d2847c7125a240a0e8411c8602aca9fb797
Muhammad Junaid <muhammadjunaidhassan1@gmail.com>
2019-03-19 03:56:09 +0500

    recently showned

--- src/components/recently-played/recently-played.ts
+++ src/components/recently-played/recently-played.ts
@@ -6,6 +6,8 @@ import { Song } from '../../data/Song';
 
 import { Shuffler } from '../../data/Helpers/Shuffler';
 import { SongsInitializer } from '../../data/Initializers/SongsInitializer';
+import { ApiProvider } from '../../providers/api/api';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'recently-played',
@@ -13,12 +15,46 @@ import { SongsInitializer } from '../../data/Initializers/SongsInitializer';
 })
 export class RecentlyPlayedComponent {
   recentlyPlayedSongs: Song[] = [];
+  recentlySongs;
+  songs;
+  recent;
 
-  constructor(@Inject(forwardRef(() => MusicPlayerPageService)) public musicPlayerPageService: MusicPlayerPageService) {
+  constructor(@Inject(forwardRef(() => MusicPlayerPageService)) public musicPlayerPageService: MusicPlayerPageService,
+  private api: ApiProvider) {
     console.log('Hello RecentlyPlayedComponent Component');
 
     this.recentlyPlayedSongs = Shuffler.shuffle(
       SongsInitializer.songs.slice()
     ).slice(0, 6);
+
+   this.getData();
+  }
+
+  getData(){
+    this.api.getAllSongs()
+      .pipe(map(actions => actions.map(a => {
+        const data = a.payload.doc.data();
+        const did = a.payload.doc.id;
+        return {did, ...data};
+      })))
+        .subscribe(res =>{
+          this.songs = res;
+          this.api.getRecentlyPlayed(localStorage.getItem('uid'))
+          .pipe(map(actions => actions.map(a =>{
+            const data = a.payload.doc.data();
+            const did = a.payload.doc.id;
+            return {did, ...data};
+          })))
+            .subscribe(res =>{
+              this.recentlySongs = res;
+               this.setRecentSongs();
+            })
+        })
+  }
+
+  setRecentSongs(){
+    if(this.recentlySongs.length !== 0){
+      this.recent = this.songs.filter( data => this.recentlySongs[0].songs.indexOf(data.did)> -1);
+    }
   }
 }
